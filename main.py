63ffc96c8968fe1dd431310e813acce53e4e830a
Lakshya Dhingra <dlakshya223@gmail.com>
2025-01-06 13:54:28 +0530

    Shows table with existing database

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import sys
 
 from PyQt6.QtGui import QAction
 from PyQt6.QtWidgets import QApplication, \
-    QLabel, QWidget, QGridLayout, QLineEdit, QPushButton, \
-    QMainWindow
+    QMainWindow, QTableWidget, QTableWidgetItem
+
+import sqlite3
 
 
 class MainWindow(QMainWindow):
@@ -21,8 +22,27 @@ class MainWindow(QMainWindow):
         help_menu_item.addAction(about_action)
         about_action.setMenuRole(QAction.MenuRole.NoRole)
 
+    # Creating table using QTableWidget
+        self.table = QTableWidget()
+        self.table.setColumnCount(4)
+        self.table.setHorizontalHeaderLabels(("ID", "Name", "Course", "Mobile No."))
+        # Remove duplicate index column
+        self.table.verticalHeader().setVisible(False)
+        self.setCentralWidget(self.table)
+
+    def load_data(self):
+        connection = sqlite3.connect("database.db")
+        result = connection.execute("SELECT * FROM students")
+        self.table.setRowCount(0)
+        for row_number, row_data in enumerate(result):
+            self.table.insertRow(row_number)
+            for column_number, column_data in enumerate(row_data):
+                self.table.setItem(row_number, column_number, QTableWidgetItem(str(column_data)))
+        connection.close()
+
 
 app = QApplication(sys.argv)
-age_calculator = MainWindow()
-age_calculator.show()
+student_database = MainWindow()
+student_database.show()
+student_database.load_data()
 sys.exit(app.exec())
